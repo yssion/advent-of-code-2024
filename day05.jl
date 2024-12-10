bcedb81f413ad49f11799000c19ade69ae81194d
yssion <michellewang326@gmail.com>
2024-12-10 12:29:01 +0100

    Added day 05 part 2

--- day05.jl
+++ day05.jl
@@ -1,31 +1,17 @@
-function is_ordered(rules, update)
-    for (i, page) in enumerate(update)
-        before = rules[findall(rules[:, 2] .== page), 1]
-        any(n in update[i:end] for n in before) && return false
-    end
-    return true
-end
-
-function reorder(rules, update)
-    # identify which entries are problematic
-
-end
-
-data = readlines("input/test.txt")
-# data = readlines("input/day05.txt")
+data = readlines("input/day05.txt")
 sep = findfirst(data .== "")
 rules = stack([parse.(Int, split(data[i], '|')) for i=1:sep-1], dims=1)
 updates = [parse.(Int, split(data[i], ',')) for i=sep+1:length(data)]
 
-# Changes all invalid updates to list of zeroes and then sum middle entries
-valid_updates = [is_ordered(rules, update) for update in updates] .* updates
-sol1 = sum(update[div(length(update)+1, 2)] for update in valid_updates)
+is_before(x, y) = any([x, y] == rules[i, 1:2] for i in 1:rules.size[1])
+is_ordered(v) = all(is_before(v[i], v[j]) for j in eachindex(v) for i=1:j-1)
 
-for update in updates
-    if !is_ordered(rules, update)
-        new = reorder(rules, update)
-    end
-end
+# Unwanted update entries are set to zero for summation
+valid_updates = [is_ordered(update) for update in updates]
+sol1 = sum(update[div(length(update)+1, 2)] for update in valid_updates .* updates)
+
+newly_sorted = [sort(update, lt=is_before) for update in (valid_updates .== 0) .* updates]
+sol2 = sum(update[div(length(update)+1, 2)] for update in newly_sorted)
 
 println("Solution 1: ", sol1)
-# println("Solution 2: ", sol2)
+println("Solution 2: ", sol2)
